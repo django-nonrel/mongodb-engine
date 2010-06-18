81f15b1e9303de2e9c6644c041a0317711d04de3
Jonas Haag <jonas@lophus.org>
2010-06-18 18:16:19 +0200

    Implemented lazy database connection.

--- django_mongodb_engine/mongodb/base.py
+++ django_mongodb_engine/mongodb/base.py
@@ -32,33 +32,39 @@ class DatabaseIntrospection(NonrelDatabaseIntrospection):
 class DatabaseWrapper(NonrelDatabaseWrapper):
     def __init__(self, *args, **kwds):
         super(DatabaseWrapper, self).__init__(*args, **kwds)
-        settings_dict = self.settings_dict
-        NAME = settings_dict["NAME"]
-        HOST = settings_dict["HOST"]
-        try:
-            PORT = int(settings_dict["PORT"])
-        except ValueError:
-            raise ImproperlyConfigured("PORT must be an integer, or a string "
-                                       "which is easily convertable to an "
-                                       "integer")
-
-        USER = settings_dict["USER"]
-        PASSWORD = settings_dict["PASSWORD"]
-        connection = pymongo.Connection(HOST, PORT, slave_okay=True)
-        if USER and PASSWORD:
-            auth = connection['admin'].authenticate(USER, PASSWORD)
-            if not auth:
-                raise ImproperlyConfigured("Username and/or password for \
-the mongoDB are not correct")
-        from .mongodb_serializer import TransformDjango
-        self._connection = connection
-        self.db_name = NAME
-        self.db_connection = connection[NAME]
-        self.db_connection.add_son_manipulator(TransformDjango())
-
         self.features = DatabaseFeatures(self)
         self.ops = DatabaseOperations(self)
         self.client = DatabaseClient(self)
         self.creation = DatabaseCreation(self)
         self.validation = DatabaseValidation(self)
         self.introspection = DatabaseIntrospection(self)
+        self._is_connected = False
+
+    @property
+    def db_connection(self):
+        self._ensure_is_connected()
+        return self._db_connection
+
+    def _ensure_is_connected(self):
+        if not self._is_connected:
+            settings_dict = self.settings_dict
+            try:
+                PORT = int(settings_dict["PORT"])
+            except ValueError:
+                raise ImproperlyConfigured("PORT must be an integer")
+            NAME = settings_dict["NAME"]
+            HOST = settings_dict["HOST"]
+            USER = settings_dict["USER"]
+            PASSWORD = settings_dict["PASSWORD"]
+            connection = pymongo.Connection(HOST, PORT, slave_okay=True)
+            if USER and PASSWORD:
+                auth = connection['admin'].authenticate(USER, PASSWORD)
+                if not auth:
+                    raise ImproperlyConfigured("Username and/or password for "
+                                               "the MongoDB are not correct")
+            from .mongodb_serializer import TransformDjango
+            self._connection = connection
+            self.db_name = NAME
+            self._db_connection = connection[NAME]
+            self._db_connection.add_son_manipulator(TransformDjango())
+            self._is_connected = True
