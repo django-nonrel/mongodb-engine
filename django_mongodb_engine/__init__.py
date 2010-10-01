e1eb285185926d9334900e2304a7e07a788df908
Jonas Haag <jonas@lophus.org>
2010-10-01 14:45:05 +0200

    Model signals are now automatically connected on mongodb-engine import

--- django_mongodb_engine/__init__.py
+++ django_mongodb_engine/__init__.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import logging
-logger = logging.getLogger(__name__)
-
 __version__     = (0, 1, 2)
 __author__      = "Flavio Percoco Premoli, Alberto Paro and contributors"
 __contact__     = "django-mongodb-engine@lophus.org"
 __homepage__    = "http://github.com/django-mongodb-engine/mongodb-engine"
 __docformat__   = "restructuredtext"
+
+import _bootstrap
--- django_mongodb_engine/models.py
+++ django_mongodb_engine/models.py
@@ -1,5 +1,3 @@
-from django.db.models import signals
-from .mongodb.fields import add_mongodb_manager, pre_init_mongodb_signal
-
-signals.pre_init.connect(pre_init_mongodb_signal)
-signals.class_prepared.connect(add_mongodb_manager)
+import warning
+warning.warn("The django_mongodb_engine.models module is deprecated. " \
+             "Please remove your import.", DeprecationWarning)
