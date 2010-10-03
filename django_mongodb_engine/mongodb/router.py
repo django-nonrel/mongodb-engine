58642da65bcf6b4cd5bfe6fa8049799db98bed06
Jonas Haag <jonas@lophus.org>
2010-10-03 13:30:30 +0200

    Cleaned up MongoDBRouter

--- django_mongodb_engine/mongodb/router.py
+++ django_mongodb_engine/mongodb/router.py
@@ -1,79 +1,64 @@
+from django.conf import settings
+from django.exceptions import ImproperlyConfigured
+
+def model_label(model):
+    return '%s.%s' % (model._meta.app_label, model._meta.module_name)
 
 class MongoDBRouter(object):
-    """A router to control all database operations on models in
-    the myapp application"""
+    """
+    A router to control all database operations on models in the myapp application
+    """
     def __init__(self):
-        from django.conf import settings
-        self.managed_apps = [app.split('.')[-1] for app in getattr(settings, "MONGODB_MANAGED_APPS", [])]
-        self.managed_models = getattr(settings, "MONGODB_MANAGED_MODELS", [])
-        self.mongodb_database = None
-        self.mongodb_databases = []
+        self.managed_apps = [app.split('.')[-1] for app in getattr(settings, 'MONGODB_MANAGED_APPS', [])]
+        self.managed_models = getattr(settings, 'MONGODB_MANAGED_MODELS', [])
+        self.mongodb_database, self.mongodb_databases = self.get_databases()
 
-        for name, databaseopt in settings.DATABASES.items():
-            if databaseopt["ENGINE"]=='django_mongodb_engine.mongodb':
-                if databaseopt.get("IS_DEFAULT", False) and not self.mongodb_database:
-                    self.mongodb_database = name
-                elif databaseopt.get("IS_DEFAULT", False) and self.mongodb_database:
-                    raise RuntimeError("There can be just one mongodb default db")
-
+    def get_databases(self):
+        default_database = None
+        databases = []
+        for name, databaseopt in settings.DATABASES.iteritems():
+            if databaseopt['ENGINE'] == 'django_mongodb_engine.mongodb':
                 self.mongodb_databases.append(name)
+                if databaseopt.get('IS_DEFAULT'):
+                    if default_database is None:
+                        default_database = name
+                    else:
+                        raise ImproperlyConfigured("There an be only one default MongoDB database")
 
-        if self.mongodb_database is None and not self.mongodb_databases:
-            raise RuntimeError("A mongodb database must be set")
-        self.mongodb_database = self.mongodb_databases[0]
+        if not databases:
+            raise ImproperlyConfigured("No MongoDB database found in settings.DATABASES")
+
+        if default_database is None:
+            default_database = databases[0]
+
+        return default_database, databases
+
+    def model_app_is_managed(self, model):
+        return model._meta.app_label in self.managed_apps
+
+    def model_is_managed(self, model):
+        return model_label(model) in self.managed_models
+
+    def is_managed(self, model):
+        return self.model_app_is_managed(model) or self.model_is_managed(model)
 
     def db_for_read(self, model, **hints):
-        "Point all operations on mongodb models to a mongodb database"
-        if model._meta.app_label in self.managed_apps:
+        """Point all operations on mongodb models to a mongodb database"""
+        if self.is_managed(model):
             return self.mongodb_database
-        key = "%s.%s"%(model._meta.app_label, model._meta.module_name)
-        if key in self.managed_models:
-            return self.mongodb_database
-        return None
 
-    def db_for_write(self, model, **hints):
-        "Point all operations on mongodb models to a mongodb database"
-        if model._meta.app_label in self.managed_apps:
-            return self.mongodb_database
-        key = "%s.%s"%(model._meta.app_label, model._meta.module_name)
-        if key in self.managed_models:
-            return self.mongodb_database
-        return None
+    db_for_write = db_for_read # same algorithm
 
     def allow_relation(self, obj1, obj2, **hints):
-        "Allow any relation if a model in myapp is involved"
-
-        #key1 = "%s.%s"%(obj1._meta.app_label, obj1._meta.module_name)
-        key2 = "%s.%s"%(obj2._meta.app_label, obj2._meta.module_name)
-
-        # obj2 is the model instance so, mongo_serializer should take care
-        # of the related object. We keep trac of the obj1 db so, don't worry
-        # about the multi-database management
-        if obj2._meta.app_label in self.managed_apps or key2 in self.managed_models:
-            return True
-
-        return None
+        """Allow any relation if a model in myapp is involved"""
+        return self.is_managed(obj2)
 
     def allow_syncdb(self, db, model):
-        "Make sure that a mongodb model appears on a mongodb database"
-        key = "%s.%s"%(model._meta.app_label, model._meta.module_name)
-        if db in self.mongodb_databases:
-            return model._meta.app_label  in self.managed_apps or key in self.managed_models
-        elif model._meta.app_label in self.managed_apps or key in self.managed_models:
-            if db in self.mongodb_databases:
-                return True
-            else:
-                return False
-        return None
+        """Make sure that a mongodb model appears on a mongodb database"""
+        return db in self.mongodb_databases and self.is_managed(model)
 
     def valid_for_db_engine(self, driver, model):
-        "Make sure that a model is valid for a database provider"
-        if driver!="mongodb":
-            return None
-        if model._meta.app_label in self.managed_apps:
-            return True
-        key = "%s.%s"%(model._meta.app_label, model._meta.module_name)
-        if key in self.managed_models:
-            return True
-        return None
-
+        """Make sure that a model is valid for a database provider"""
+        if driver != 'mongodb':
+            return False
+        return self.is_managed(model)
