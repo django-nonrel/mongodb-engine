ca21e1d85da9c199271307c57dce74c201128ac8
Jonas Haag <jonas@lophus.org>
2010-10-04 16:13:57 +0200

    Update bootstrapping stuff.

--- django_mongodb_engine/_bootstrap.py
+++ django_mongodb_engine/_bootstrap.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db.models import signals
-from .fields import add_mongodb_manager, pre_init_mongodb_signal
+from .mongodb.fields import add_mongodb_manager, pre_init_mongodb_signal
 
 if 'django_mongodb_engine' not in settings.INSTALLED_APPS:
     settings.INSTALLED_APPS.insert(0, 'django_mongodb_engine')
--- django_mongodb_engine/models.py
+++ django_mongodb_engine/models.py
@@ -1,5 +1,4 @@
 import mongodb._bootstrap
-if False:
-    import warnings
-    warnings.warn("The django_mongodb_engine.models module is deprecated. " \
-                  "Please remove your import.", DeprecationWarning)
+import warnings
+warnings.warn("The django_mongodb_engine.models module is deprecated. " \
+              "Please remove your import.", DeprecationWarning)
