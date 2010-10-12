a2de1a260c034d1361e8ab8b317fbab748f413e5
Flavio Percoco Premoli <flaper87@gmail.com>
2010-10-12 19:36:36 +0200

    Removed Autofield patch

--- django_mongodb_engine/_bootstrap.py
+++ django_mongodb_engine/_bootstrap.py
@@ -8,41 +8,8 @@ from django.db import models
 from django.db.models.fields import AutoField as DjangoAutoField
 from pymongo.objectid import ObjectId
 
-def AutoField_to_python(value):
-    if value is None:
-        return value
-    try:
-        return unicode(value)
-    except (TypeError, ValueError):
-        raise exceptions.ValidationError("Invalid AutoField value %r" % value)
-
-def AutoField_get_prep_value(value):
-    if value is None:
-        return None
-    return ObjectId(value)
-
 class MongoMeta(object):
     pass
-    
-def pre_init_mongodb_signal(sender, args, **kwargs):
-    model = sender # may not use `model` directly as argument because of
-                   # Django's magic arg inspecting voodoo
-    if model._meta.abstract:
-        return
-
-    database_for_model = settings.DATABASES[model.objects.db]
-    primary_key_field  = model._meta.pk
-
-    if (
-        'mongodb' in database_for_model['ENGINE']          # model doesn't use MongoDB backend
-        and isinstance(primary_key_field, DjangoAutoField) # custom primary key
-        and not modified_django                            # already patched Django version XXX
-    ):
-        # patch the default AutoField to make it ObjectId-compatible
-        setattr(primary_key_field, 'to_python', AutoField_to_python)
-        setattr(primary_key_field, 'get_prep_value', AutoField_get_prep_value)
-
-models.signals.pre_init.connect(pre_init_mongodb_signal)
 
 def class_prepared_mongodb_signal(sender, *args, **kwargs):
     model = sender
