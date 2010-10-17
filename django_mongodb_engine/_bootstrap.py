af93474baba96f6e08fc7d893df53798a43eb2c8
Jonas Haag <jonas@lophus.org>
2010-10-17 18:31:17 +0200

    Fixed and cleaned up _bootstrap

--- django_mongodb_engine/_bootstrap.py
+++ django_mongodb_engine/_bootstrap.py
@@ -1,22 +1,10 @@
-try:
-    from django import MODIFIED as modified_django
-except ImportError:
-    modified_django = False
-from django.core import exceptions
-from django.conf import settings
 from django.db import models
-from django.db.models.fields import AutoField as DjangoAutoField
-from pymongo.objectid import ObjectId
-
-class MongoMeta(object):
-    pass
 
 def class_prepared_mongodb_signal(sender, *args, **kwargs):
-    model = sender
-    mongo_meta = getattr(cls, "MongoMeta", MongoMeta).__dict__.copy()
-    for attr in mongo_meta:
-        if attr.startswith("_"):
-            continue
-        setattr(model._meta, attr, mongo_meta[attr])
+    mongo_meta = getattr(sender, 'MongoMeta', None)
+    if mongo_meta is not None:
+        for attr, value in mongo_meta.items():
+            if not attr.startswith('_'):
+                setattr(model._meta, attr, value)
 
 models.signals.class_prepared.connect(class_prepared_mongodb_signal)
