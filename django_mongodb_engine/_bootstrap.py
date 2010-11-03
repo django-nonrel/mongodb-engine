7600a14b04a86285c7a96cbde188f0effa216234
Jonas Haag <jonas@lophus.org>
2010-11-03 23:17:07 +0100

    Fixed MongoMeta

--- django_mongodb_engine/_bootstrap.py
+++ django_mongodb_engine/_bootstrap.py
@@ -3,8 +3,8 @@ from django.db import models
 def class_prepared_mongodb_signal(sender, *args, **kwargs):
     mongo_meta = getattr(sender, 'MongoMeta', None)
     if mongo_meta is not None:
-        for attr, value in mongo_meta.items():
+        for attr in dir(mongo_meta):
             if not attr.startswith('_'):
-                setattr(sender._meta, attr, value)
+                setattr(sender._meta, attr, getattr(mongo_meta, attr))
 
 models.signals.class_prepared.connect(class_prepared_mongodb_signal)
