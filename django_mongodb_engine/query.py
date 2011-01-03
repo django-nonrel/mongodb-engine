bf10deec31f587efc2908b4909263a47ab60f261
Jonas Haag <jonas@lophus.org>
2011-01-03 02:06:50 +0100

    Wrong error class

--- django_mongodb_engine/query.py
+++ django_mongodb_engine/query.py
@@ -2,7 +2,7 @@ from djangotoolbox.fields import AbstractIterableField, EmbeddedModelField
 
 class A(object):
     def __init__(self, op, value):
-        raise NotImplemented("Sorry, A() queries/updates are broken")
+        raise NotImplementedError("Sorry, A() queries/updates are broken")
         self.op = op
         self.val = value
 
