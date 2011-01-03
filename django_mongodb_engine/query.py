b1d25e3908e57b05d8a360ff7def1d175bd04033
Jonas Haag <jonas@lophus.org>
2011-01-03 02:01:38 +0100

    Add note about A() being broken (will be fixed in next release)

--- django_mongodb_engine/query.py
+++ django_mongodb_engine/query.py
@@ -2,6 +2,7 @@ from djangotoolbox.fields import AbstractIterableField, EmbeddedModelField
 
 class A(object):
     def __init__(self, op, value):
+        raise NotImplemented("Sorry, A() queries/updates are broken")
         self.op = op
         self.val = value
 
