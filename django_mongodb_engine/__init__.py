efa17b61e102b121cb436d3213fb5a6e8baff6b8
Flavio Percoco Premoli <flaper87@gmail.com>
2011-01-13 09:36:22 +0100

    Forgot to change version

--- django_mongodb_engine/__init__.py
+++ django_mongodb_engine/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = (0, 2, 0)
+__version__ = (0, 3, 0)
 __author__ = "Flavio Percoco Premoli, Alberto Paro, " + \
              "Jonas Haag and contributors"
 __contact__ = "django-non-relational@googlegroups.com"
