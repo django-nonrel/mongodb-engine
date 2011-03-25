14c61c26eda0bf105bbdf577d2bc286022e91184
Jonas Haag <jonas@lophus.org>
2011-03-25 16:06:38 +0100

    Fixed sqlite test settings and added mongodb database + router

--- tests/settings_sqlite.py
+++ tests/settings_sqlite.py
@@ -6,5 +6,10 @@ DATABASES = {
         'ENGINE' : 'sqlite3',
     }
 }
-for app in ['embedded', 'search', 'storage']:
+for app in ['embedded', 'storage']:
     INSTALLED_APPS.remove(app)
+
+DATABASES['mongodb'] = {'NAME' : 'mongodb', 'ENGINE' : 'django_mongodb_engine'}
+
+DATABASE_ROUTERS = ['django_mongodb_engine.router.MongoDBRouter']
+MONGODB_MANAGED_APPS = ['general']
