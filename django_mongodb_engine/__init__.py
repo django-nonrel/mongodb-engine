f5fa76a646d5aeb7b498675206c85a586ad0bc5a
Jonas Haag <jonas@lophus.org>
2011-05-23 15:32:58 +0200

    0.4 is ready!

--- django_mongodb_engine/__init__.py
+++ django_mongodb_engine/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = (0, 3, 0)
+__version__ = (0, 4, 0)
 __author__ = "Flavio Percoco Premoli, Alberto Paro, " + \
              "Jonas Haag and contributors"
 __contact__ = "django-non-relational@googlegroups.com"
-__homepage__ = "https://github.com/django-mongodb-engine/mongodb-engine"
+__homepage__ = "https://django-mongodb.org"
 __docformat__ = "restructuredtext"
 
 try:
