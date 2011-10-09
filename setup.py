a67919292f0c056553a978bfd7999a8275727c1d
Jonas Haag <jonas@lophus.org>
2011-10-09 16:36:29 +0200

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 from setuptools import setup, find_packages
-import codecs
 import django_mongodb_engine as distmeta
-
 
 CLASSIFIERS = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
     'Topic :: Internet',
     'Topic :: Database',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Operating System :: OS Independent',
 ]
-
-for ver in ['2', '2.4', '2.5', '2.6', '2.7']:
-    CLASSIFIERS.append('Programming Language :: Python :: %s' % ver)
-
 
 setup(
     name='django-mongodb-engine',
@@ -24,12 +21,8 @@ setup(
     author_email=distmeta.__contact__,
     url=distmeta.__homepage__,
     license='2-clause BSD',
-    description= "A MongoDB backend standing outside django.",
-    long_description=codecs.open('README.rst', 'r', 'utf-8').read(),
-
-    platforms=['any'],
+    description= "MongoDB backend for Django",
     install_requires=['pymongo', 'djangotoolbox'],
-
     packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
     include_package_data=True,
     classifiers=CLASSIFIERS,
