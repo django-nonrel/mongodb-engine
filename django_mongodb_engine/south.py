12b85a35067d27af981b2fc348bf8104fa221d01
Simon Charette <charette.s@gmail.com>
2012-03-04 02:27:23 -0500

    Added add, delete, rename column.

--- django_mongodb_engine/south.py
+++ django_mongodb_engine/south.py
@@ -2,12 +2,13 @@
 # TODO: This module should be renamed to south_adapter
 from __future__ import absolute_import 
 
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models.fields import NOT_PROVIDED
+from django.db.utils import IntegrityError
 from pymongo.errors import DuplicateKeyError
 
-from django.core.exceptions import ImproperlyConfigured
-from django.db.utils import IntegrityError
-
 from .utils import make_index_list
+
 
 try:
     from south.db.generic import DatabaseOperations
@@ -30,21 +31,27 @@ class DatabaseOperations(DatabaseOperations):
     def _get_collection(self, name):
         return self._get_connection().get_collection(name)
 
-    def add_column(self, table_name, name, field, *args, **kwds):
-        #TODO: maybe we should use $unset for existing documents?
-        pass
+    def add_column(self, table_name, field_name, field, keep_default=True):
+        collection = self._get_collection(table_name)
+        __, name = field.get_attname_column()
+        if field.default is not NOT_PROVIDED:
+            default = field.default() if callable(field.default) else field.default
+            # Update all the documents that have not this field yet
+            collection.update({name: {'$exists': False}}, {'$set': {name: default}})
+            if not keep_default:
+                field.default = NOT_PROVIDED
 
-    def alter_column(self, table_name, name, field, explicit_name=True):
+    def alter_column(self, table_name, column_name, field, explicit_name=True):
         # There's not much we can do here
         pass
 
-    def delete_column(self, table_name, column_name):
-        #TODO: use $unset
-        pass
+    def delete_column(self, table_name, name):
+        collection = self._get_collection(table_name)
+        collection.update(dict(), {'$unset': {name: 1}})
 
     def rename_column(self, table_name, old, new):
-        #TODO: use $rename
-        pass
+        collection = self._get_collection(table_name)
+        collection.update(dict(), {'$rename': {old: new}})
 
     def create_unique(self, table_name, columns, drop_dups=False):
         collection = self._get_collection(table_name)
