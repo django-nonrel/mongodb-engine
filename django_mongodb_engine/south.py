17d0cff572986d0fb50f7d5cbe4d1b8bdbd09224
Simon Charette <charette.s@gmail.com>
2012-03-04 00:14:23 -0500

    Initial support for create,delete unique and create,rename,delete table

--- django_mongodb_engine/south.py
+++ django_mongodb_engine/south.py
@@ -1,4 +1,22 @@
-class DatabaseOperations(object):
+# What a great idea it was to name this module south...
+# TODO: This module should be renamed to south_adapter
+from __future__ import absolute_import 
+
+from pymongo.errors import DuplicateKeyError
+
+from django.core.exceptions import ImproperlyConfigured
+from django.db.utils import IntegrityError
+
+from .utils import make_index_list
+
+try:
+    from south.db.generic import DatabaseOperations
+except ImportError:
+    raise ImproperlyConfigured('Make sure to install south before trying to '
+                               'import this module.')
+
+
+class DatabaseOperations(DatabaseOperations):
     """
     MongoDB implementation of database operations.
     """
@@ -9,32 +27,54 @@ class DatabaseOperations(object):
     has_check_constraints = False
     has_ddl_transactions = False
 
-    def __init__(self, db_alias):
-        pass
+    def _get_collection(self, name):
+        return self._get_connection().get_collection(name)
 
     def add_column(self, table_name, name, field, *args, **kwds):
+        #TODO: maybe we should use $unset for existing documents?
         pass
 
     def alter_column(self, table_name, name, field, explicit_name=True):
+        # There's not much we can do here
         pass
 
     def delete_column(self, table_name, column_name):
+        #TODO: use $unset
         pass
 
     def rename_column(self, table_name, old, new):
+        #TODO: use $rename
         pass
 
-    def create_unique(self, table_name, columns):
-        pass
+    def create_unique(self, table_name, columns, drop_dups=False):
+        collection = self._get_collection(table_name)
+        try:
+            index_list = list(make_index_list(columns))
+            collection.create_index(index_list, unique=True, drop_dups=drop_dups)
+        except DuplicateKeyError as e:
+            raise IntegrityError(e)
 
     def delete_unique(self, table_name, columns):
-        pass
+        collection = self._get_collection(table_name)
+        index_list = list(make_index_list(columns))
+        collection.drop_index(index_list)
 
     def delete_primary_key(self, table_name):
         pass
 
+    def create_table(self, table_name, fields, **kwargs):
+        # Collection creation is automatic but code calling this might expect
+        # it to exist, thus we create it here.
+        connection = self._get_connection()
+        connection.database.create_collection(table_name, **kwargs)
+    
+    def rename_table(self, table_name, new_table_name):
+        collection = self._get_collection(table_name)
+        collection.rename(new_table_name)
+
     def delete_table(self, table_name, cascade=True):
-        pass
+        connection = self._get_connection()
+        connection.database.drop_collection(table_name)
 
     def connection_init(self):
         pass
@@ -55,9 +95,6 @@ class DatabaseOperations(object):
         pass
 
     def clear_run_data(self, pending_creates):
-        pass
-
-    def create_table(self, unique=True, null=True, blank=True):
         pass
 
     def send_create_signal(self, verbosity=False, interactive=False):
