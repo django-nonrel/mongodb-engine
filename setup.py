29535c75dc3b960e8677e95aeeb3345cd8f2e6a8
Alex Burgel <aburgel@gmail.com>
2013-11-28 13:02:59 -0500

    Change setup.py to use distutils

--- setup.py
+++ setup.py
@@ -1,33 +1,35 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+import os
 
 import django_mongodb_engine as distmeta
 
+def get_packages(package):
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
-CLASSIFIERS = [
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2.5',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Topic :: Internet',
-    'Topic :: Database',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Operating System :: OS Independent',
-]
-
-
-setup(
-    name='django-mongodb-engine',
+setup(name='django-mongodb-engine',
     version='.'.join(map(str, distmeta.__version__)),
     author=distmeta.__author__,
     author_email=distmeta.__contact__,
     url=distmeta.__homepage__,
     license='2-clause BSD',
     description="MongoDB backend for Django",
-    install_requires=['pymongo', 'djangotoolbox'],
-    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
-    include_package_data=True,
-    classifiers=CLASSIFIERS,
-    test_suite='tests',
+    requires=['pymongo', 'djangotoolbox (>=1.6.0)'],
+    packages=get_packages('django_mongodb_engine'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
