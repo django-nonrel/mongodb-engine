0904a94b173e10c907fec5b6df99abfc0bbdb757
Tristen Georgiou <tristen.georgiou@gmail.com>
2014-08-25 08:41:26 -0700

    Started adding in support for Mongo geospatial fields.

--- django_mongodb_engine/fields.py
+++ django_mongodb_engine/fields.py
@@ -1,4 +1,10 @@
+import json
+from django.contrib.gis import forms
+from django.contrib.gis.db.models.proxy import GeometryProxy
+from django.contrib.gis.geometry.backend import Geometry
 from django.db import connections, models
+from django.utils import six
+from django.utils.translation import ugettext_lazy as _
 
 from gridfs import GridFS
 from gridfs.errors import NoFile
@@ -172,3 +178,145 @@ try:
         [], ['^django_mongodb_engine\.fields\.GridFSString'])
 except ImportError:
     pass
+
+
+class GeometryField(models.Field):
+    """
+    The base GIS field -- maps to the OpenGIS Specification Geometry type.
+
+    Based loosely on django.contrib.gis.db.models.fields.GeometryField
+    """
+
+    # The OpenGIS Geometry name.
+    geom_type = 'GEOMETRY'
+    form_class = forms.GeometryField
+
+    lookup_types = {
+        'within':     '$geoWithin',
+        'intersects': '$geoIntersects',
+        'near':       '$near',
+        'nearsphere': '$nearSphere',
+    }
+
+    description = _("The base GIS field -- maps to the OpenGIS Specification Geometry type.")
+
+    def __init__(self, verbose_name=None, dim=2, **kwargs):
+        """
+        The initialization function for geometry fields.  Takes the following
+        as keyword arguments:
+
+        dim:
+         The number of dimensions for this geometry.  Defaults to 2.
+        """
+
+        # Mongo GeoJSON are WGS84
+        self.srid = 4326
+
+        # Setting the dimension of the geometry field.
+        self.dim = dim
+
+        # Setting the verbose_name keyword argument with the positional
+        # first parameter, so this works like normal fields.
+        kwargs['verbose_name'] = verbose_name
+
+        super(GeometryField, self).__init__(**kwargs)
+
+    def to_python(self, value):
+        if isinstance(value, Geometry):
+            return value
+        elif isinstance(value, dict):
+            return Geometry(json.dumps(value), self.srid)
+        elif isinstance(value, (bytes, six.string_types)):
+            return Geometry(value, self.srid)
+        raise ValueError('Could not convert to python geometry from value type "%s".' % type(value))
+
+    def get_prep_value(self, value):
+        if isinstance(value, Geometry):
+            return json.loads(value.json())
+        elif isinstance(value, six.string_types):
+            return json.loads(value)
+        raise ValueError('Could not prep geometry from value type "%s".' % type(value))
+
+    def get_srid(self, geom):
+        """
+        Returns the default SRID for the given geometry, taking into account
+        the SRID set for the field.  For example, if the input geometry
+        has no SRID, then that of the field will be returned.
+        """
+        gsrid = geom.srid  # SRID of given geometry.
+        if gsrid is None or self.srid == -1 or (gsrid == -1 and self.srid != -1):
+            return self.srid
+        else:
+            return gsrid
+
+    ### Routines overloaded from Field ###
+    def contribute_to_class(self, cls, name, virtual_only=False):
+        super(GeometryField, self).contribute_to_class(cls, name, virtual_only)
+
+        # Setup for lazy-instantiated Geometry object.
+        setattr(cls, self.attname, GeometryProxy(Geometry, self))
+
+    def db_type(self, connection):
+        return self.geom_type
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': self.form_class,
+                    'geom_type': self.geom_type,
+                    'srid': self.srid,
+                    }
+        defaults.update(kwargs)
+        if (self.dim > 2 and not 'widget' in kwargs and
+                not getattr(defaults['form_class'].widget, 'supports_3d', False)):
+            defaults['widget'] = forms.Textarea
+        return super(GeometryField, self).formfield(**defaults)
+
+    def get_db_prep_lookup(self, lookup_type, value, connection, prepared=False):
+        if lookup_type not in self.lookup_types:
+            raise ValueError('Unknown lookup type "%s".' % lookup_type)
+        if not isinstance(value, Geometry):
+            raise ValueError('Geometry value is of unsupported type "%s".' % type(value))
+        # TODO: extra params???
+        return {self.lookup_types[lookup_type]: {'$geometry': json.loads(value.json())}}
+
+
+# The OpenGIS Geometry Type Fields
+class PointField(GeometryField):
+    geom_type = 'POINT'
+    form_class = forms.PointField
+    description = _("Point")
+
+
+class LineStringField(GeometryField):
+    geom_type = 'LINESTRING'
+    form_class = forms.LineStringField
+    description = _("Line string")
+
+
+class PolygonField(GeometryField):
+    geom_type = 'POLYGON'
+    form_class = forms.PolygonField
+    description = _("Polygon")
+
+
+class MultiPointField(GeometryField):
+    geom_type = 'MULTIPOINT'
+    form_class = forms.MultiPointField
+    description = _("Multi-point")
+
+
+class MultiLineStringField(GeometryField):
+    geom_type = 'MULTILINESTRING'
+    form_class = forms.MultiLineStringField
+    description = _("Multi-line string")
+
+
+class MultiPolygonField(GeometryField):
+    geom_type = 'MULTIPOLYGON'
+    form_class = forms.MultiPolygonField
+    description = _("Multi polygon")
+
+
+class GeometryCollectionField(GeometryField):
+    geom_type = 'GEOMETRYCOLLECTION'
+    form_class = forms.GeometryCollectionField
+    description = _("Geometry collection")
